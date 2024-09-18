3437f05498be57e860ae8cb2f67b9df9d25814cf
Sam Yeager <syeager@evtcorp.com>
2024-09-17 23:17:05 -0500

    default values as variables, index.html added

--- realestateapp/views.py
+++ realestateapp/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # Define a dictionary with all default values
+    default_values = {
+        'home_price_selected': 175000,
+        'home_price_min': 50000,
+        'home_price_max': 300000,
+        'down_payment': 30000,
+        'closing_costs_perc_min': 2,
+        'closing_costs_perc_max': 3.5,
+        'closing_costs_perc_selected': 3.5,
+        'loan_term': 30,
+        'interest_rate': 5.95,
+        'property_tax': 1.1,
+        'insurance_rate': 0.5,
+        'hoa': 0,
+        'gas': 0,
+        'electricity': 0,
+        'internet': 0,
+        'water_trash': 0,
+        'contribution': 0,
+        'savings_goal': 50000,
+    }
+    
+    context = {
+        'defaults': default_values,
+    }
+    return render(request, 'index.html', context)
